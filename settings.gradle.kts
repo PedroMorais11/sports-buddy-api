82e4867cf3588c543e4dcfe087fdd194d6757c97
Pedro <pedromtmorais@gmail.com>
2020-08-31 00:21:29 +0100

    second commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "sports-buddy-api"
---
+++ src/main/kotlin/com/pedro/sportsbuddyapi/SportsBuddyApiApplication.kt
@@ -0,0 +1,11 @@
+package com.pedro.sportsbuddyapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SportsBuddyApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<SportsBuddyApiApplication>(*args)
+}
